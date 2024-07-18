45efa73883fd97d74f57af4cb1a1c416892bf685
abhisheknagargoje <abhinagargoje63@gmail.com>
2024-07-18 13:23:03 +0530

    fix home page route

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -13,11 +13,11 @@ const Heading = () => {
 						<p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
 							Intra College Academic Helper for all your academic needs!
 						</p>
-						<a
-							href="/doubts"
+						<Link
+							to="/doubts"
 							className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
 						>
-							<Link to="/doubts">Doubts</Link>
+							Doubts
 							<svg
 								className="w-5 h-5 ml-2 -mr-1"
 								fill="currentColor"
@@ -25,12 +25,12 @@ const Heading = () => {
 								xmlns="http://www.w3.org/2000/svg"
 							>
 								<path
-									fill-rule="evenodd"
+									fillRule="evenodd"
 									d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-									clip-rule="evenodd"
+									clipRule="evenodd"
 								></path>
 							</svg>
-						</a>
+						</Link>
 						<a
 							href="/meetings"
 							className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
